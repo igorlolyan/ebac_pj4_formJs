840cbda0f16476771e266a1e6e32fdf7aa14cc43
igorlolyan <igor.lol.yan@gmail.com>
2023-10-06 17:20:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+
+//Impedir a página de atualizar
+const form = document.getElementById("Formulário")
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+})
+
+
+
+form.addEventListener("submit", function(e){
+    //Pegando os valores inseridos no input
+    const A = parseFloat(document.getElementById("campo-A").value)
+    const B = parseFloat(document.getElementById("campo-B").value)
+    
+    if (typeof A === "number" && typeof B === "number"){
+        if ( A > B) {
+            alert("Formulário Inválido")
+        }
+        if (A < B){
+            alert("Formulário Válido")
+        }
+        if (A === B){
+            alert("Formulário Inválido - Números iguáis")
+        }
+    else {
+        alert("Digite números válidos")
+    }}
+})
